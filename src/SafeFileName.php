48040565537d56167ea84c7b7a30bc513bbf1d10
Simbiat <simbiat@bk.ru>
2021-02-21 13:01:43 +0300

    Adjustments

--- src/SafeFileName.php
+++ src/SafeFileName.php
@@ -5,85 +5,116 @@ namespace SafeFileName;
 class SafeFileName
 {
     private array $needles = [];
+    private array $needles_ext = [];
     private array $replaces = [
-        #Remove control characters
-        '/[[:cntrl:]]/i'=>'',
+        #Remove control characters and whitespaces excep regular space
+        '/[[:cntrl:]]/iu' => '',
+        #Replace whitespace with regular space (hex 20)
+        '/[\r\n\t\f\v\0\x{00A0}\x{2002}-\x{200B}\x{202F}\x{205F}\x{3000}\x{FEFF}]/iu' => ' ',
         #Replace characters with fullwidth alternatives
-        '/</i'=>'＜',
-        '/>/i'=>'＞',
-        '/:/i'=>'：',
-        '/"/i'=>'＂',
-        '/\//i'=>'／',
-        '/\\\\/i'=>'＼',
-        '/\|/i'=>'｜',
-        '/\?/i'=>'？',
-        '/\*/i'=>'＊',
+        '/</iu' => '＜',
+        '/>/iu' => '＞',
+        '/:/iu' => '：',
+        '/"/iu' => '＂',
+        '/\//iu' => '／',
+        '/\\\\/iu' => '＼',
+        '/\|/iu' => '｜',
+        '/\?/iu' => '？',
+        '/\*/iu' => '＊',
         #Replace Windows specific reserved words while retaining case
-        '/^(CON)(\.*.*)$/'=>'ＣＯＮ$2',
-        '/^(con)(\.*.*)$/'=>'ｃｏｎ$2',
-        '/^(COn)(\.*.*)$/'=>'ＣＯｎ$2',
-        '/^(CoN)(\.*.*)$/'=>'ＣｏＮ$2',
-        '/^(Con)(\.*.*)$/'=>'Ｃｏｎ$2',
-        '/^(cON)(\.*.*)$/'=>'ｃＯＮ$2',
-        '/^(COn)(\.*.*)$/'=>'ＣＯｎ$2',
-        '/^(coN)(\.*.*)$/'=>'ｃｏＮ$2',
-        '/^(PRN)(\.*.*)$/'=>'ＰＲＮ$2',
-        '/^(prn)(\.*.*)$/'=>'ｐｒｎ$2',
-        '/^(PRn)(\.*.*)$/'=>'ＰＲｎ$2',
-        '/^(PrN)(\.*.*)$/'=>'ＰｒＮ$2',
-        '/^(Prn)(\.*.*)$/'=>'Ｐｒｎ$2',
-        '/^(pRN)(\.*.*)$/'=>'ｐＲＮ$2',
-        '/^(PRn)(\.*.*)$/'=>'ＰＲｎ$2',
-        '/^(prN)(\.*.*)$/'=>'ｐｒＮ$2',
-        '/^(AUX)(\.*.*)$/'=>'ＡＵＸ$2',
-        '/^(aux)(\.*.*)$/'=>'ａｕｘ$2',
-        '/^(AUx)(\.*.*)$/'=>'ＡＵｘ$2',
-        '/^(AuX)(\.*.*)$/'=>'ＡｕＸ$2',
-        '/^(Aux)(\.*.*)$/'=>'Ａｕｘ$2',
-        '/^(aUX)(\.*.*)$/'=>'ａＵＸ$2',
-        '/^(AUx)(\.*.*)$/'=>'ＡＵｘ$2',
-        '/^(auX)(\.*.*)$/'=>'ａｕＸ$2',
-        '/^(NUL)(\.*.*)$/'=>'ＮＵＬ$2',
-        '/^(nul)(\.*.*)$/'=>'ｎｕｌ$2',
-        '/^(NUl)(\.*.*)$/'=>'ＮＵｌ$2',
-        '/^(NuL)(\.*.*)$/'=>'ＮｕＬ$2',
-        '/^(Nul)(\.*.*)$/'=>'Ｎｕｌ$2',
-        '/^(nUL)(\.*.*)$/'=>'ｎＵＬ$2',
-        '/^(NUl)(\.*.*)$/'=>'ＮＵｌ$2',
-        '/^(nuL)(\.*.*)$/'=>'ｎｕＬ$2',
-        '/^(COM)(\d{1,})(\.*.*)$/'=>'ＣＯＭ$2$3',
-        '/^(com)(\d{1,})(\.*.*)$/'=>'ｃｏｍ$2$3',
-        '/^(COm)(\d{1,})(\.*.*)$/'=>'ＣＯｍ$2$3',
-        '/^(CoM)(\d{1,})(\.*.*)$/'=>'ＣｏＭ$2$3',
-        '/^(Com)(\d{1,})(\.*.*)$/'=>'Ｃｏｍ$2$3',
-        '/^(cOM)(\d{1,})(\.*.*)$/'=>'ｃＯＭ$2$3',
-        '/^(COm)(\d{1,})(\.*.*)$/'=>'ＣＯｍ$2$3',
-        '/^(coM)(\d{1,})(\.*.*)$/'=>'ｃｏＭ$2$3',
-        '/^(LPT)(\d{1,})(\.*.*)$/'=>'ＬＰＴ$2$3',
-        '/^(lpt)(\d{1,})(\.*.*)$/'=>'ｌｐｔ$2$3',
-        '/^(LPt)(\d{1,})(\.*.*)$/'=>'ＬＰｔ$2$3',
-        '/^(LpT)(\d{1,})(\.*.*)$/'=>'ＬｐＴ$2$3',
-        '/^(Lpt)(\d{1,})(\.*.*)$/'=>'Ｌｐｔ$2$3',
-        '/^(lPT)(\d{1,})(\.*.*)$/'=>'ｌＰＴ$2$3',
-        '/^(LPt)(\d{1,})(\.*.*)$/'=>'ＬＰｔ$2$3',
-        '/^(lpT)(\d{1,})(\.*.*)$/'=>'ｌｐＴ$2$3',
+        '/^(CON)(\.*.*)$/u' => 'ＣＯＮ$2',
+        '/^(con)(\.*.*)$/u' => 'ｃｏｎ$2',
+        '/^(COn)(\.*.*)$/u' => 'ＣＯｎ$2',
+        '/^(CoN)(\.*.*)$/u' => 'ＣｏＮ$2',
+        '/^(Con)(\.*.*)$/u' => 'Ｃｏｎ$2',
+        '/^(cON)(\.*.*)$/u' => 'ｃＯＮ$2',
+        '/^(COn)(\.*.*)$/u' => 'ＣＯｎ$2',
+        '/^(coN)(\.*.*)$/u' => 'ｃｏＮ$2',
+        '/^(PRN)(\.*.*)$/u' => 'ＰＲＮ$2',
+        '/^(prn)(\.*.*)$/u' => 'ｐｒｎ$2',
+        '/^(PRn)(\.*.*)$/u' => 'ＰＲｎ$2',
+        '/^(PrN)(\.*.*)$/u' => 'ＰｒＮ$2',
+        '/^(Prn)(\.*.*)$/u' => 'Ｐｒｎ$2',
+        '/^(pRN)(\.*.*)$/u' => 'ｐＲＮ$2',
+        '/^(PRn)(\.*.*)$/u' => 'ＰＲｎ$2',
+        '/^(prN)(\.*.*)$/u' => 'ｐｒＮ$2',
+        '/^(AUX)(\.*.*)$/u' => 'ＡＵＸ$2',
+        '/^(aux)(\.*.*)$/u' => 'ａｕｘ$2',
+        '/^(AUx)(\.*.*)$/u' => 'ＡＵｘ$2',
+        '/^(AuX)(\.*.*)$/u' => 'ＡｕＸ$2',
+        '/^(Aux)(\.*.*)$/u' => 'Ａｕｘ$2',
+        '/^(aUX)(\.*.*)$/u' => 'ａＵＸ$2',
+        '/^(AUx)(\.*.*)$/u' => 'ＡＵｘ$2',
+        '/^(auX)(\.*.*)$/u' => 'ａｕＸ$2',
+        '/^(NUL)(\.*.*)$/u' => 'ＮＵＬ$2',
+        '/^(nul)(\.*.*)$/u' => 'ｎｕｌ$2',
+        '/^(NUl)(\.*.*)$/u' => 'ＮＵｌ$2',
+        '/^(NuL)(\.*.*)$/u' => 'ＮｕＬ$2',
+        '/^(Nul)(\.*.*)$/u' => 'Ｎｕｌ$2',
+        '/^(nUL)(\.*.*)$/u' => 'ｎＵＬ$2',
+        '/^(NUl)(\.*.*)$/u' => 'ＮＵｌ$2',
+        '/^(nuL)(\.*.*)$/u' => 'ｎｕＬ$2',
+        '/^(COM)(\d{1,})(\.*.*)$/u' => 'ＣＯＭ$2$3',
+        '/^(com)(\d{1,})(\.*.*)$/u' => 'ｃｏｍ$2$3',
+        '/^(COm)(\d{1,})(\.*.*)$/u' => 'ＣＯｍ$2$3',
+        '/^(CoM)(\d{1,})(\.*.*)$/u' => 'ＣｏＭ$2$3',
+        '/^(Com)(\d{1,})(\.*.*)$/u' => 'Ｃｏｍ$2$3',
+        '/^(cOM)(\d{1,})(\.*.*)$/u' => 'ｃＯＭ$2$3',
+        '/^(COm)(\d{1,})(\.*.*)$/u' => 'ＣＯｍ$2$3',
+        '/^(coM)(\d{1,})(\.*.*)$/u' => 'ｃｏＭ$2$3',
+        '/^(LPT)(\d{1,})(\.*.*)$/u' => 'ＬＰＴ$2$3',
+        '/^(lpt)(\d{1,})(\.*.*)$/u' => 'ｌｐｔ$2$3',
+        '/^(LPt)(\d{1,})(\.*.*)$/u' => 'ＬＰｔ$2$3',
+        '/^(LpT)(\d{1,})(\.*.*)$/u' => 'ＬｐＴ$2$3',
+        '/^(Lpt)(\d{1,})(\.*.*)$/u' => 'Ｌｐｔ$2$3',
+        '/^(lPT)(\d{1,})(\.*.*)$/u' => 'ｌＰＴ$2$3',
+        '/^(LPt)(\d{1,})(\.*.*)$/u' => 'ＬＰｔ$2$3',
+        '/^(lpT)(\d{1,})(\.*.*)$/u' => 'ｌｐＴ$2$3',
+    ];
+    #Some more characters, that you might want to replace with fullwidth alternatives, depedning on how you use the files
+    private $replaces_ext = [
+        '/\[/iu' => '［',
+        '/\]/iu' => '］',
+        '/=/iu' => '＝',
+        '/;/iu' => '；',
+        '/,/iu' => '，',
+        '/&/iu' => '＆',
+        '/\$/iu' => '＄',
+        '/#/iu' => '＃',
+        '/\(/iu' => '（',
+        '/\)/iu' => '）',
+        '/\~/iu' => '～',
+        '/\`/iu' => '｀',
+        '/\'/iu' => '＇',
+        '/\!/iu' => '！',
+        '/\{/iu' => '｛',
+        '/\}/iu' => '｝',
+        '/%/iu' => '％',
+        '/\+/iu' => '＋',
+        '/‘/iu' => '＇',
+        '/’/iu' => '＇',
+        '/«/iu' => '＂',
+        '/»/iu' => '＂',
+        '/”/iu' => '＂',
+        '/“/iu' => '＂',
     ];
     
     public function __construct()
     {
-        #Setting needles (characters to search for) in order not to duplicate the values
+        #Setting needles (characters to search for) in order not to duplicate the values and also cache them, in case we need to use the class for multiple strings
         $this->needles = array_keys($this->replaces);
+        $this->needles_ext = array_keys($this->replaces_ext);
     }
     
-    public function sanitize(string $string): string
+    public function sanitize(string $string, bool $extended = true): string
     {
         #Replace special characters
         $string = preg_replace($this->needles, $this->replaces, $string);
-        #Remove spaces and dots from right
-        $string = rtrim(rtrim($string), '.');
-        if $string === '' {
-            throw new \UnexpectedValueException('Resulting string has no characters.');
+        if ($extended) {
+            $string = preg_replace($this->needles_ext, $this->replaces_ext, $string);
         }
+        #Remove spaces and dots from right (spaces on the left are possible
+        $string = rtrim(rtrim(rtrim($string), '.'));
         return $string;
     }
 }
